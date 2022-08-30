933fbd25f6b4b6e6bdfe88f343fa05fc0f3015c9
Alvaro11G <agarcia11746@gmail.com>
2022-08-30 15:47:07 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var answer = 4;
+
+           var response = (answer < 9) ? "is less than nine." : "greater than or equal to nine.";
+
+            Console.WriteLine($"The response you got back is: {response}");
+        }
+    }
+}
